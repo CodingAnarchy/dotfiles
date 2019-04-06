2c25517e65e113c2f6a0b67e9df80cd8e49e9d15
Matt Tanous <mtanous22@gmail.com>
2019-04-05 21:15:32 -0400

    Include missing dotfiles

---
+++ fish/abbreviations.fish
@@ -0,0 +1,46 @@
+# Apache
+abbr a2reload 'sudo service apache2 reload'
+abbr a2restart 'sudo service apache2 restart'
+
+# Git
+abbr g 'git'
+abbr ga. 'git add .'
+abbr ga 'git add'
+abbr gcm 'git commit -m'
+abbr gco 'git checkout'
+abbr gi 'gitignore'
+abbr gm 'git merge'
+abbr gpl 'git pull'
+abbr gps 'git push'
+abbr gs 'git status'
+abbr gc 'git clone'
+abbr gd 'git diff'
+
+# LS
+abbr l 'ls'
+
+# NPM
+abbr ni 'npm install --save-dev'
+abbr nig 'npm install -g'
+abbr np 'npm prune'
+abbr nl 'npm list'
+abbr nr 'npm remove'
+abbr nu 'npm update'
+
+# PGREP
+abbr pg 'pgrep -l'
+
+# Symfony
+abbr s 'php app/console'
+abbr scc 'php app/console cache:clear'
+abbr sgb 'php app/console generate:bundle'
+abbr sgc 'php app/console generate:controller'
+abbr sdge 'php app/console doctrine:generate:entity'
+abbr sdsc 'php app/console doctrine:schema:create'
+abbr sdsu 'php app/console doctrine:schema:update'
+abbr sdsv 'php app/console doctrine:schema:validate'
+abbr sddc 'php app/console doctrine:database:create'
+
+# Other
+abbr add_to_path 'set -U fish_user_paths $fish_user_paths'
+abbr freebox_mount 'sudo mount -t cifs //mafreebox.freebox.fr/Disque\\ dur /media/freebox -o user=freebox,uid=1000,gid=1000,rw,password='

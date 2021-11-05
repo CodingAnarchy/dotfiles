80b071ec5d3f2c1453148c69b379fbf9f4156192
Matt Tanous <mtanous22@gmail.com>
2021-11-05 14:06:09 -0400

    Update fish config to current usage

--- fish/config.fish
+++ fish/config.fish
@@ -1,19 +1,15 @@
 set -x SHELL /usr/local/bin/fish
 set -x GOPATH $HOME/.go
-set -x PATH /usr/local/opt/coreutils/libexec/gnubin /usr/local/sbin $PATH $GOPATH/bin ~/bin
+set -x PATH /usr/local/sbin /usr/local/opt/coreutils/libexec/gnubin $PATH $GOPATH/bin ~/bin
 set -x MANPATH /usr/local/opt/coreutils/libexec/gnuman $MANPATH
 set -x EDITOR vim
-source {$HOME}/.github
-set -x JAVA_HOME (/usr/libexec/java_home -v 1.8)
+set -x HOMEBREW_BOOTSNAP 1
+
 set -x GPG_TTY (tty)
 set -e fish_greeting
 
 if [ -f /usr/share/autojump/autojump.fish ]
   source /usr/share/autojump/autojump.fish
-end
-
-function nvm
-  bass source (brew --prefix nvm)/nvm.sh --no-use ';' nvm $argv
 end
 
 source ~/.config/fish/abbreviations.fish
@@ -24,3 +20,5 @@ start_ssh_agent
 test -e {$HOME}/.iterm2_shell_integration.fish ; and source {$HOME}/.iterm2_shell_integration.fish
 
 brew command command-not-found-init > /dev/null 2>&1; and . (brew command-not-found-init)
+
+[ -f "~/.config/fish/conf.d/canary.fish" ]; and source "~/.config/fish/conf.d/canary.fish"
